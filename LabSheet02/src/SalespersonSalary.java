8dd2ceb39558d75040962383da38491083411b2b
PariwatRom <pariwatrompan@gmail.com>
2024-12-04 11:19:47 +0700

    LabSheet02

--- LabSheet02/src/SalespersonSalary.java
+++ LabSheet02/src/SalespersonSalary.java
@@ -1,8 +1,32 @@
-
+import java.util.Scanner;
 public class SalespersonSalary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		// Declare constants
+		 final double BADE_SALARY = 1000.0;
+		 final double COMMISSION_RATE = 0.15;
+		 final int SENTINEL = -1; // Terminating value for input
+		 // Declare variables
+		 int sales; // Input gross sales
+		 double salary; // Salary to be computed
+		
+		while (true) {
+			System.out.print("Enter gross sales for the salesperson (or -1 to end): ");
+			sales = input.nextInt();
+			
+			if(sales==SENTINEL) {
+				break;
+			}
+			salary = BADE_SALARY+(sales*COMMISSION_RATE);
+			
+			System.out.printf("The saleperson's salary is : $%.2f%n%n",salary);
+		
+			System.out.printf("bye");
+		}
+		
+		input.close();
 
 	}
 
