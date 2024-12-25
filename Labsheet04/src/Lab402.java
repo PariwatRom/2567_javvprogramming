8a42c852310d35f5ba94e4bc126d89e9ed1bd8c9
PariwatRom <pariwatrompan@gmail.com>
2024-12-25 09:25:21 +0700

    LabSheet04-MethodHomework

--- Labsheet04/src/Lab402.java
+++ Labsheet04/src/Lab402.java
@@ -1,9 +1,32 @@
+import javax.swing.*;
 
 public class Lab402 {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Input year:");
+        int year = Integer.parseInt(input);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        if (checkYear(year)) {
+            if (isLeapYear(year)) {
+                JOptionPane.showMessageDialog(null, year + " is Leap year.");
+            } else {
+                JOptionPane.showMessageDialog(null, year + " is NOT Leap year.");
+            }
+        } else {
+            
+        }
+    }
 
-	}
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            }
+            return true;
+        }
+        return false;
+    }
 
+    public static boolean checkYear(int year) {
+        return year >= 1000 && year <= 3000;
+    }
 }
